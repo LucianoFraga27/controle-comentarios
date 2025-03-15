0ccd2c0cc5c3761575edca633cb7d0d2db9c0d48
lucianolucas <luciano.lucas@bllc.com.br>
2025-03-15 01:09:40 -0300

    frontend

---
+++ frontend/app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:app/home/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({ super.key });
+
+   @override
+   Widget build(BuildContext context) {
+       return GetMaterialApp(
+        title: "Conexão Api Python",
+        getPages: [
+          GetPage(name: "/", page: () => const HomePage())
+        ],
+       );
+  }
+}
